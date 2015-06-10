e6ffefe7d66d966510e90eed93bfc01c828552d4
Clive <augustinclive@hotmail.com>
2015-06-10 07:57:26 -0400

    add Profile route

--- app.rb
+++ app.rb
@@ -215,7 +215,7 @@ class CreditCardService < Sinatra::Base
     haml :store
   end
 
-  get '/user/:username', :auth => [:user] do
+  get '/user/:username' do
     username = params[:username]
     unless username == @current_user.username
       flash[:error] = "You may only look at your own profile"
