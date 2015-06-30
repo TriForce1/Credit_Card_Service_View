7bf08c4bdbf6a5c85ae9cc9fd081904f0bc94a34
Clive <augustinclive@hotmail.com>
2015-06-30 02:34:41 -0400

    MEMCACHIER

--- helpers/creditcard_helper.rb
+++ helpers/creditcard_helper.rb
@@ -35,9 +35,9 @@ module CreditCardHelper
   end
 
   def api_card_index
-    HTTParty.get("#{API_URL_BASE}/api/v1/credit_card?user_id=#{@current_user.id}",
+    JSON.parse(HTTParty.get("#{API_URL_BASE}/api/v1/credit_card?user_id=#{@current_user.id}",
     :headers  => {'Content-Type' => 'application/json', 'Accept' => 'application/json', 'authorization' => ('Bearer ' + user_jwt)
-    })
+    })))
     # url = API_URL+'operation'
     # headers = {'authorization' => ('Bearer ' + user_jwt)}
     # HTTParty.get url, headers: headers
